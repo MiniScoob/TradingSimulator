255ca848ccde6e31e17d58f05928f68ff46b830d
MiniScoob <55055972+MiniScoob@users.noreply.github.com>
2021-09-04 16:56:11 +0700

    Initial Commit

---
+++ TradingSimulator/TradingSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingSimulatorApp.swift
+//  TradingSimulator
+//
+//  Created by Владислав Попов on 04.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TradingSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
